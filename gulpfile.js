3bc5de2802fd9d5edbf45f09b0de0d096611129d
Matt Derrick <git@mattderrick.co.uk>
2015-09-06 14:24:31 +0100

    Dokku/Heroku setup, lazy CI

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 'use strict';
 
-require('./tasks/build');
+var gulp = require('gulp');
+var build = require('./tasks/build');
+
 require('./tasks/serve');
 require('./tasks/editorconfig');
+
+// I'm being lazy here. I will set up CI later but for now
+// I am going to build on the same box that serves the site :/
+gulp.task('heroku', build);
